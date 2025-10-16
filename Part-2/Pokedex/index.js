8f143e66928e2c71bc59602bc124017865587ff8
shadrackbond <stellamusembi10@gmail.com>
2025-10-16 17:28:58 +0300

    completed working on the pokemon application, having learnt lots of css and also implemented a fetch API and using the async/await

---
+++ Part-2/Pokedex/index.js
@@ -0,0 +1,22 @@
+const getPokemon = async () =>{
+    try{
+        const pokemonName = document.getElementById('searchName').value.toLowerCase();
+        const pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if(!pokemonData.ok){
+            throw new Error("could not find pokemon");
+        }
+
+        const data = await pokemonData.json();
+        const pokemonImage = data.sprites.front_default;
+
+        const displayPokemon = document.getElementById('pokemonImg');
+
+        displayPokemon.src = pokemonImage;
+        displayPokemon.style.display = 'block';
+
+    }
+    catch(error){
+        alert(error);
+    }
+}
