db3f90fb4de5b1417e169597ee703c2e883e1f58
shadrackbond <stellamusembi10@gmail.com>
2025-10-30 08:47:38 +0300

    working on a fizzBuzz challenge from hackerRank

--- TestRuns/app.js
+++ TestRuns/app.js
@@ -1,11 +1,22 @@
-const readline = require("readline");
+function fizzBuzz(n) {
+    // Write your code here
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+    for (let i=0; i < n; i++) {
+        let z;
+        let i = z;
+        let divisible3 = z % 3;
+        let divisible5 = z % 5;
+        if (divisible3 || divisible5) {
+            console.log("FizzBuzz");
+        } else if (divisible3 || !divisible5) {
+            console.log("Fizz");
+        } else if (!divisible3 || divisible5) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
 
-//Uncomment this block to pass the first stage
-rl.question("$ ", (answer) => {
-    rl.close();
-});
+}
+
+fizzBuzz(15)
